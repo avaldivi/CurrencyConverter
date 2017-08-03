2cc49b046da94f7818051722e2b9fcfb7f5949e0
Adrianna Valdivia <avaldivi@ggc.edu>
2017-08-03 15:42:29 -0400

    Add logo component and styling

--- App/screens/Home.js
+++ App/screens/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
-import { View, StatusBar } from 'react-native';
-import Container from '../components/container/Container';
+import { StatusBar } from 'react-native';
+import { Container } from '../components/container';
+import { Logo } from '../components/logo';
 
 export default () => (
   <Container>
-    <StatusBar
-      translucent={false}
-      barStyle="light-content" 
-    />
-    <View/>
+    <StatusBar translucent={false} barStyle="light-content" />
+    <Logo />
   </Container>
 );
